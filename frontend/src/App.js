105808a49d3be9de5e7547f865d6dca20b279ef0
dineshpandey <dineshpandey3536@gmail.com>
2024-02-07 10:36:48 -0600

    created Signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./output.css";
 import LoginComponent from './routes/Login';
+import SignupComponent from './routes/Signup';
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <Routes>
         {/* <Route path="/hi" element={<div>hiiiii</div>} /> */}
         <Route path="/login" element={<LoginComponent />} />
+        <Route path="/signup" element={<SignupComponent />} />
+
       </Routes>
       </BrowserRouter>
   );
