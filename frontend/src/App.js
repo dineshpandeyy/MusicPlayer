329f4eaed236d484052ec5387d35df314dc62606
dineshpandey <dineshpandey3536@gmail.com>
2024-02-07 00:24:33 -0600

    add tailwind CSS, React Router and Login Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./output.css";
+import LoginComponent from './routes/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+      <Routes>
+        {/* <Route path="/hi" element={<div>hiiiii</div>} /> */}
+        <Route path="/login" element={<LoginComponent />} />
+      </Routes>
+      </BrowserRouter>
   );
 }
 
