bb1a3a4a0324eb2e10933fa42be2b756cecd6389
dineshpandey <dineshpandey3536@gmail.com>
2024-02-13 23:04:32 -0600

    Added HomePage and HomePage sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./output.css";
 import LoginComponent from './routes/Login';
 import SignupComponent from './routes/Signup';
+import HomeComponent from "./routes/Home";
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
         {/* <Route path="/hi" element={<div>hiiiii</div>} /> */}
         <Route path="/login" element={<LoginComponent />} />
         <Route path="/signup" element={<SignupComponent />} />
+        <Route path="/home" element={<HomeComponent />} />
+
+        
 
       </Routes>
       </BrowserRouter>
